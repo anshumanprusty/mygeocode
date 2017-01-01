4a5dda56db7a7d44e37b6670025ea43684434376
Anshuman Prusty <anshuman.prusty@gmail.com>
2017-01-01 23:45:43 +0530

    First Commit

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,9 @@
+class PlacesController < ApplicationController
+  def show
+  	@places = HTTParty.get("http://localhost:3000/places/index")
+    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
+      marker.lat place["latitude"]
+      marker.lng place["longitude"]
+     end 	
+  end
+end
